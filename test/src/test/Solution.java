adce6f0a3937bca2422482b90fc3d1275fa26f91
anandkpai <anand.pai@datatechnos.com>
2020-06-01 16:00:01 -0400

    added in place quicksort test

--- test/src/test/Solution.java
+++ test/src/test/Solution.java
@@ -1,36 +1,51 @@
 package test;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Random;
 
+public class Solution {
 
-public class Solution 
-{
 	static Random rand 	= new Random();
-	static final int DEFAULT_ARRAY_CAPACITY = 100;
-	static final int MAX_RANDOM_INT_VALUE   = 5000;
-		
-	public static void main(String[] args) {
-		int[] dupes = new int[DEFAULT_ARRAY_CAPACITY] ;
-		System.out.printf("before filling \t%6d\n",dupes.length);
-		
-		Random rand = new Random();
-		for (int i=0;i<DEFAULT_ARRAY_CAPACITY;i++) dupes[i] = rand.nextInt(MAX_RANDOM_INT_VALUE);
-		System.out.printf("after filling \t%6d\n",dupes.length);
-		
-		HashSet<Integer>  vals  = new HashSet<Integer>(DEFAULT_ARRAY_CAPACITY);
-		boolean found 			= false;										
-		for (int i=0;i<DEFAULT_ARRAY_CAPACITY;i++) {
-			if (vals.contains(dupes[i])) {
-				System.out.printf("found \t%6d\n", dupes[i]);
-				found = true;
-				break;
-			}
-			else vals.add(dupes[i]);
-		}
-		if (!found) System.out.println("no duplicates found");
+	static final int ARRAYLEN  = 10000000;
+	static final int MAXINT    = 1000000;
+	
 
+	
+
+	
+	public static void main(String[] args) {
+		SortArray  mstest = new SortArray(ARRAYLEN);
+		SortArray  qtest  = mstest.clone();
+		SortArray  intest = mstest.clone();
+		SortArray  ref    = mstest.clone();
+		
+		long start;
+		start = System.currentTimeMillis();
+		ref.data.sort(null);
+		long reftime = System.currentTimeMillis()-start;
+		System.out.printf("reference time \t%6d\n",reftime);
+
+		assert (ref.data != qtest.data);	
+		start = System.currentTimeMillis();		
+		qtest.quickSort();
+		long qtime = System.currentTimeMillis()-start;		
+		assert (ref.data == qtest.data);
+		System.out.printf("quicksort \t%6d   \t%6.2fx\n",qtime, ((float)qtime/reftime));
+						
+		assert (ref.data != mstest.data);	
+		start = System.currentTimeMillis();
+		mstest.mergeSort();
+		long mstime = System.currentTimeMillis()-start;		
+		assert (ref.data == mstest.data);		
+		System.out.printf("mergesort \t%6d   \t%6.2fx\n",mstime, (((float)mstime)/reftime));
+		
+		assert (ref.data != intest.data);	
+		start = System.currentTimeMillis();
+		intest.inplaceQuickSort(0, intest.data.size()-1);
+		long intime = System.currentTimeMillis()-start;		
+		assert (ref.data == intest.data);		
+		System.out.printf("inplaceqSort\t%6d   \t%6.2fx\n", intime, (((float)intime)/reftime));					
+		
 	}
+
 }
