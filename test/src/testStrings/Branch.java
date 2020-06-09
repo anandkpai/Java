091e008f71628c4ea02431a8da6feaccc9447a76
anandkpai <anand.pai@datatechnos.com>
2020-06-09 10:12:51 -0400

    Allow all threads

--- test/src/testStrings/Branch.java
+++ test/src/testStrings/Branch.java
@@ -4,31 +4,48 @@ import java.util.HashSet;
 
 
 
-public class Branch extends Tree{
+public class Branch extends Tree implements Runnable {
 	
 	private Branch parent;
 	private char chr;	
 	private String toString="";
-	private static final HashSet<String> nodeSignatures = new HashSet<String>();
+	private static int threadCount = 0;
+	private int index;
+	private String substr;
 	
-	protected Branch(Branch parent,int index, String substr) {
+	private Branch(){}
+	
+	protected Branch(Branch branch,String substr) {
+		this(branch,0,substr);
+		Branch.nodeSignatures.clear();
+	}
+	
+	private Branch(Branch branch,int index, String substr) {
+		final int DUMMYVALUE = 1;
 		this.chr 	= substr.charAt(index);
-		this.parent = parent;
-		if (index < substr.length()-1) new Branch(parent, index+1, substr);
+		this.parent = branch;
+		this.index  = index;
+		this.substr = substr;
+		
+		if  (Branch.threadCount<Tests.MAX_THREADS) this.run();
+		else {
+		if (index < substr.length()-1) new Branch(branch, index+1, substr);
 		
 		String nodeSignature = this.toString()+substr;
-		nodeSignature = parent==null?nodeSignature:parent.toString()+nodeSignature;
-		if (!Branch.nodeSignatures.contains(nodeSignature)){
-			Branch.nodeSignatures.add(nodeSignature);
-			if (substr.length()==1) Tree.lowest.add(this);
-			else new Branch(this,0,substr.substring(0,index)+substr.substring(index+1));
+		nodeSignature = this.parent != null?branch.toString()+nodeSignature:nodeSignature;
+		
+		if (Tree.nodeSignatures.putIfAbsent(nodeSignature, DUMMYVALUE)==null)
+			if (substr.length()==2) 
+				Tree.words.add(this.toString()+substr.substring(0,index)+substr.substring(index+1));
+			else 
+				new Branch(this,0,substr.substring(0,index)+substr.substring(index+1));
 		}
 	}
 	
 	@Override
 	public String toString() {
 		if (this.toString.length()==0) 
-			this.toString = this.parent==null?String.valueOf(this.chr):this.parent.toString()+String.valueOf(this.chr);
+			this.toString = this.parent != null?this.parent.toString()+String.valueOf(this.chr):String.valueOf(this.chr);
 		return this.toString;	
 	}
 	
@@ -42,5 +59,23 @@ public class Branch extends Tree{
 		if (!(o instanceof Branch )) return false;
 		Branch other = (Branch)o;
 		return this.toString().hashCode()==other.toString().hashCode()?true:false;
+	}
+
+	@Override
+	public void run() {
+		final int DUMMYVALUE = 1;
+		Branch.threadCount++;
+		Branch branch = this.parent;
+		if (index < substr.length()-1) new Branch(branch, index+1, substr);
+		
+		String nodeSignature = this.toString()+substr;
+		nodeSignature = this.parent != null?branch.toString()+nodeSignature:nodeSignature;
+		
+		if (Tree.nodeSignatures.putIfAbsent(nodeSignature, DUMMYVALUE)==null)
+			if (substr.length()==2) 
+				Tree.words.add(this.toString()+substr.substring(0,index)+substr.substring(index+1));
+			else 
+				new Branch(this,0,substr.substring(0,index)+substr.substring(index+1));		
+		Branch.threadCount--;
 	}	
 }
